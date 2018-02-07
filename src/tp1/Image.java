e196b6c1759a3902d9c7ff73805f93ff3e353a95
DESKTOP-JVU1CAL\mndus <dussault.mn@gmail.com>
2018-02-07 10:11:30 -0500

    image

--- src/tp1/Image.java
+++ src/tp1/Image.java
@@ -14,4 +14,36 @@ public class Image {
     int dimY;
     int resol;
     Pixel [][] _matrice;
+
+    public void setDimX(int dimX) {
+        this.dimX = dimX;
+    }
+
+    public void setDimY(int dimY) {
+        this.dimY = dimY;
+    }
+
+    public void setResol(int resol) {
+        this.resol = resol;
+    }
+
+    public void setMatrice(Pixel[][] _matrice) {
+        this._matrice = _matrice;
+    }
+
+    public int getDimX() {
+        return dimX;
+    }
+
+    public int getDimY() {
+        return dimY;
+    }
+
+    public int getResol() {
+        return resol;
+    }
+
+    public Pixel[][] getMatrice() {
+        return _matrice;
+    }
 }
