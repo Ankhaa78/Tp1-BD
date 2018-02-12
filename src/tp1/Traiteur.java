4f58889c51cf5a39d2c330c8874b41b89b553003
LEIF-2\razan <somethingrazan@gmail.com>
2018-02-12 14:23:47 -0500

    Pixel

--- src/tp1/Traiteur.java
+++ src/tp1/Traiteur.java
@@ -1,7 +1,5 @@
 package tp1;
 
-import java.util.Comparator;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
